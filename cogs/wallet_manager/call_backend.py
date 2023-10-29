e3f06aca02437bf96859b2a706e48a6e40a4b902
Zeal5 <tassadiqshakeel5@gmail.com>
2023-10-30 04:00:17 +0500

    wallet manager complete

--- cogs/wallet_manager/call_backend.py
+++ cogs/wallet_manager/call_backend.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union, Dict
+from typing import Optional, Union, Dict, List
 from pydantic import BaseModel
 from aiohttp import ClientResponse
 from . import configs
@@ -8,33 +8,43 @@ from . import configs
 
 class UserWallet(BaseModel):
     tg_id: int
-    secret: Optional[str]
+    secret: Optional[str] = None
 
 
 class Wallet(BaseModel):
     wallet_name: str
-    secret: str
     address: str
+    secret: Optional[str] = None
+    is_active: Optional[bool] = None
+    id: Optional[int] = None
+
 
 
 class WalletError(BaseModel):
     reason: str
 
 
-async def success_ok(response: dict) -> Wallet:
+async def success_ok(response: dict) -> Union[Wallet, List[Wallet]]:
+    print(f"response from success_ok = {response}")
+    if isinstance(response, list):
+        wallets = []
+        for i in response:
+            wallets.append(Wallet(**i))
+        return wallets
     return Wallet(**response)
 
 
 async def not_success(response: ClientResponse) -> WalletError:
     res = await response.json()
+    print(f"json response from backend = {res}")
     match response.status:
         case 400:
-            return WalletError(reason=f"{res}")
+            return WalletError(reason=f"{res['detail']}")
         case _:
             return WalletError(reason="Unkown error")
 
 
-async def make_request(url: str, data: Dict) -> Union[Wallet, WalletError]:
+async def make_request(url: str, data: Dict) -> Union[List[Wallet], Wallet, WalletError]:
     async with aiohttp.ClientSession() as Session:
         async with Session.post(f"{url}", json=data) as response:
             print(response.status)
@@ -42,6 +52,12 @@ async def make_request(url: str, data: Dict) -> Union[Wallet, WalletError]:
                 return await success_ok(await response.json())
             return await not_success(response)
 
+async def get_all_wallets(tg_id : int) -> List[Wallet] | WalletError | Wallet :
+    url = configs['url']['get_wallets']
+    data = UserWallet(tg_id = tg_id)
+    _data = data.model_dump(mode='json')
+    response = await make_request(url, _data)
+    return response 
 
 async def create_user_account(
     tg_id: int, secret: Optional[str] = None
---
+++ cogs/wallet_manager/views.py
@@ -0,0 +1,112 @@
+# from discord.ext import commands
+from discord import app_commands, Embed, colour
+from discord.ext import commands
+import discord
+from discord.ui import Button, View
+from typing import Optional, Dict, Any, Coroutine, Union
+from .call_backend import create_user_account, Wallet, WalletError
+from discord import ui
+from . import bot_settings
+import asyncio
+
+def clean_message(backend_response : Union[Wallet,WalletError]) -> Embed:
+    if isinstance(backend_response, WalletError):
+            embed = Embed(
+            title = "Failed to create new wallet",
+            description =f"{backend_response.reason}",
+            color = colour.Colour.red()
+        )
+        
+    elif isinstance(backend_response, Wallet):
+        embed = Embed(
+            title="New Wallet has been created",
+            description=f"{backend_response.wallet_name}",
+            color=colour.Colour.green(),
+        )
+        embed.add_field(
+            name="Address", value=f"```{backend_response.address}```", inline=False
+        )
+        embed.add_field(
+            name="Secret", value=f"```{backend_response.secret}```", inline=False
+        )
+    return embed 
+
+class RecoveryPhrasePrompt(ui.Modal, title="Create Wallet"):
+    secret = ui.TextInput(label="Wallet Secret")
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        print(f"user is {interaction.user.id}")
+        backend_response = await create_user_account(
+            interaction.user.id, secret= str(self.secret)
+        )  # TODO take secret as input
+        print(backend_response)
+        print(type(backend_response))
+        embed = clean_message(backend_response)
+        await interaction.user.send(embed=embed)
+        await interaction.edit_original_response(
+            embed = embed)
+            # ephemeral=True,
+            # delete_after=float(
+            #     bot_settings.get("timeout", "ephemeral_message", fallback=15)
+            # ),)
+
+    async def on_timeout(self) -> None:
+        return await super().on_timeout()
+
+
+class Dropdown(discord.ui.Select):
+    def __init__(self):
+        # Set the options that will be presented inside the dropdown
+        options = [
+            discord.SelectOption(label="1"),
+            discord.SelectOption(label="2"),
+            discord.SelectOption(label="3"),
+        ]
+
+        super().__init__(
+            placeholder="How many wallet do you want to create",
+            min_values=1,
+            max_values=1,
+            options=options,
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        # First reply after user selects how many wallets he wants to create
+        await interaction.response.edit_message(
+            content=f"Creating {self.values[0]} Wallet[s]",
+            delete_after=float(
+                bot_settings.get("timeout", "dropdown_message", fallback=300)
+            ),
+            view=None,
+        )
+        embeds_list = []
+        for _ in range(0, int(self.values[0])):
+            new_wallet: Union[Wallet, WalletError] = await create_user_account(
+                interaction.user.id
+            )
+            embeds_list.append(clean_message(new_wallet))
+
+        await interaction.user.send(
+            # content=f"{self.values[0]} New Wallet[s] have been created",
+            embeds = embeds_list,
+        )
+        embeds_with_footer = []
+        for i in embeds_list:
+           embeds_with_footer.append(i.set_footer(text = "This message will be deleted in about 5 minutes")) 
+        await interaction.edit_original_response(
+            # content=f"{self.values[0]} New Wallet[s] have been created",
+            embeds = embeds_with_footer,
+            # ephemeral= True,
+            # delete_after=float(
+            #     bot_settings.get("timeout", "original_message", fallback=300)
+            # ),
+            # # view=None,
+        )
+
+
+class DropdownView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        # Adds the dropdown to our view object.
+        self.add_item(Dropdown())
